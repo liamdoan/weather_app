fa8f5f37c4da2a57dd13a1a62f8c4e5ff4002d2c
liamdoan <lamdoan9898@gmail.com>
2021-12-17 00:49:28 +0200

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,106 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+
 function App() {
+  const api = {
+    key: "5130830e45c81fdc7596150106d37b8c",
+    base: "https://api.openweathermap.org/data/2.5/"
+  }
+  
+  let time = String(new window.Date())     
+  let date = time.slice(0,15)
+
+  const [city, setCity] = useState('')
+  const [weather, setWeather] = useState({})
+
+  const search = () => {
+    // if(e.key === "Enter") {
+     fetch(`${api.base}weather?q=${city}&units=metric&appid=${api.key}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setWeather(data);
+        setCity('');
+      }
+      )
+    // }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    search()
+  }
+
+  // const searchButton = () => {
+  //   fetch(`${api.base}weather?q=${city}&units=metric&appid=${api.key}`)
+  //   .then(res => res.json())
+  //   .then(data => {
+  //       console.log(data);
+  //       setWeather(data);
+  //       setCity('');
+  //     }
+  //   )
+  // }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={
+      (typeof weather.main != "undefined")
+      ? (       
+        (weather.main.temp >16) ? 'app warm'
+        :(weather.main.temp > 5) ? 'app cool'
+        : 'app cold'
+      )
+      : 'app'
+    }>
+      <main>
+        <form className="search-container"
+              onSubmit={handleSubmit}
+                  >
+          <input type="text"
+                  className="search-bar"
+                  placeholder="City ..."
+                  onChange={e => setCity(e.target.value)}
+                  value={city}
+          />
+          <button className="button"
+                  type="submit"
+                // onClick={searchButton}
+                >
+                   Search
+          </button>
+        </form>
+        {
+        (typeof weather.main != "undefined") 
+        ? (
+        <div className="info-container">
+          <div className="location-container">
+            <div className="location">
+              {weather.name}, {weather.sys.country}
+            </div>
+            <div className="date">
+                {date}
+            </div>
+          </div>
+          <div className="weather-container">
+            <div className="temp">
+              {Math.round(weather.main.temp)}°C
+              </div>
+            <div className="weather">
+              {weather.weather[0].description}
+            </div>
+          </div>
+        </div>
+          )         
+          : ( weather.cod ==="404") ? 
+            <div className="err-msg-wrap">
+              <p className="err-msg">City is not found ...</p>
+            </div>
+          :   
+            <p className="msg">Please select a city ...</p>
+      }
+      </main>
     </div>
   );
 }
